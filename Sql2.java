f94b92bc450ccf1cdadadcaeef11b330071f2a77
Syeda Sundas Fakhir <85859733+SSundas@users.noreply.github.com>
2021-10-26 10:12:49 -0700

    Add files via upload

--- Sql2.java
+++ Sql2.java
@@ -24,7 +24,7 @@ public static Connection getConnection () throws Exception{
 		Connection conn=DriverManager.getConnection(url,username,password);
 		//System.out.print("Connected");
 		Statement s=conn.createStatement();
-		ResultSet rs=s.executeQuery("select * from products where price='100' or seller_city='lahore' and category='3'");
+		ResultSet rs=s.executeQuery("select * from products where not seller_city='Lahore'");
 				while(rs.next())
 					System.out.println(rs.getInt(1)+"  "+ rs.getString(2)+"  "+ rs.getString(3)+"  "+ rs.getString(4)+"  "+ rs.getString(5));
 						conn.close();
