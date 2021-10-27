6c97cec9fde5bed146205fefa58e692765aa390f
Syeda Sundas Fakhir <85859733+SSundas@users.noreply.github.com>
2021-10-27 00:22:58 -0700

    Add files via upload

---
+++ DropDB.java
@@ -0,0 +1,40 @@
+package SqlJ;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DropDB {
+
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+		getConnection();
+
+	}
+
+public static Connection getConnection () throws Exception{
+	try {
+		String driver="com.mysql.cj.jdbc.Driver";
+		String url= "jdbc:mysql://127.0.0.1:3306/for_delete";
+		String username="root";
+		String password="";
+		Class.forName(driver);
+		
+		Connection conn=DriverManager.getConnection(url,username,password);
+		//System.out.print("Connected");
+		Statement s=conn.createStatement();
+		s.executeUpdate("Drop database for_delete");
+				//while(rs.next())
+					//System.out.println(rs.getInt(1));
+						conn.close();
+	    return conn;
+	}
+	   catch(Exception e)
+		{
+		   System.out.println(e); 
+		}
+		return null;
+	}
+
+}
